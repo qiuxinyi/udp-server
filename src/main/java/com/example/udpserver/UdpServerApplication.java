feec2d7ce88f4a2d535eb64b8a9f80de3ec4dab3
qiuxinyi <qiuxy@pcl.ac.cn>
2022-08-07 09:39:06 +0800

    first

---
+++ src/main/java/com/example/udpserver/UdpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.udpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdpServerApplication.class, args);
+	}
+
+}
